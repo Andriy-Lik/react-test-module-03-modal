145c27a7039a767d2c9741635ee3b0af41fc23a3
Andriy-Lik <alan321987@gmail.com>
2023-02-16 14:54:38 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import Container from './Container';
+
+class App extends Component {
+  state = {
+    showModal: false
+  };
+
+   // componentDidMount() {
+  //   const todos = localStorage.getItem('todos');
+  //   const parsedTodos = JSON.parse(todos);
+
+  //   if (parsedTodos) {
+  //     this.setState({ todos: parsedTodos });
+  //   }
+    
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (this.state.todos !== prevState.todos) {
+  //     localStorage.setItem('todos', JSON.stringify(this.state.todos));
+  //   }
+  // }
+
+  toggleModal =() => {
+    this.setState(state => ({
+      showModal: !state.showModal
+    }))
+  } 
+
+  render() {
+    const { showModal } = this.state;
+    return (
+      <Container>
+        <button type="button" onClick={this.toggleModal}>
+          Открыть модалку
+        </button>
+        {/* Если showModal=true рендерим <Modal /> */}
+        {showModal && (
+          <Modal>
+            <h1>Привет это контент модалки как children</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+              Quae cumque reiciendis pariatur deserunt tempora adipisci eaque ipsam, 
+              incidunt libero in soluta numquam commodi sapiente nulla doloribus 
+              cum saepe dicta eligendi.
+            </p>
+            <button type='button' onClick={this.toggleModal}>
+              Закрыть
+            </button>
+          </Modal>
+        )}
+        
+      </Container>
+    );
+  }
+}
+
+export default App;
